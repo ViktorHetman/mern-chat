d1dd56c6e73083591195fcad0b57b64b5f6ea0b6
Viktor <vitya.getman.79@gmail.com>
2024-04-17 20:45:00 +0200

    get messages endpoint

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
-export const sendMessage = async (req, res) => {
+const sendMessage = async (req, res) => {
   try {
     const { message } = req.body;
     const { userId: receiverId } = req.params;
@@ -27,15 +27,35 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // await conversation.save();
-    // await newMessage.save();
-
     // Will run in parallel
-    await Promise.all(conversation.save(), newMessage.save());
+    await Promise.all([conversation.save(), newMessage.save()]);
 
+    // SOCKET IO FUNCTIONALITY WILL GO HERE
     res.status(201).json(newMessage);
   } catch (error) {
-    console.log("Error in sendMessage controller", error.message);
+    console.log("Error in sendMessage controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+const getMessages = async (req, res) => {
+  try {
+    const { userId: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [receiverId, senderId] },
+    }).populate("messages"); // Not reference but actual messages
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessage controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export { sendMessage, getMessages };
--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/:userId", protectRoute, getMessages);
 router.post("/send/:userId", protectRoute, sendMessage);
 
 export default router;
