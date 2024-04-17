8070751b9bf80770fcb96e23836deb1a8cb1f612
Viktor <vitya.getman.79@gmail.com>
2024-04-17 21:08:09 +0200

    get users endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@ import express from "express";
 import rootRouter from "./root.routes.js";
 import authRouter from "./auth.routes.js";
 import messageRouter from "./message.routes.js";
+import userRouter from "./user.routes.js";
 
 const router = express.Router();
 
 router.use("/", rootRouter);
 router.use("/api/auth", authRouter);
 router.use("/api/messages", messageRouter);
+router.use("/api/users", userRouter);
 
 export default router;
